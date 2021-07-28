eaa72c98e1e2c39328e31c943bfd155888c3c92a
statiolake <statiolake@gmail.com>
2021-07-29 01:16:07 +0900

    Format by prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as child_process from 'child_process';
+import * as child_process from "child_process";
+import * as vscode from "vscode";
 
 function rangeWholeFile(doc: vscode.TextDocument): vscode.Range {
-	let lastlinum = doc.lineCount - 1;
-	let first = doc.lineAt(0).range.start.character;
-	let last = doc.lineAt(lastlinum).range.end.character;
-	return new vscode.Range(0, first, lastlinum, last);
+  let lastlinum = doc.lineCount - 1;
+  let first = doc.lineAt(0).range.start.character;
+  let last = doc.lineAt(lastlinum).range.end.character;
+  return new vscode.Range(0, first, lastlinum, last);
 }
 
 function getFormattedString(doc: vscode.TextDocument): string {
-	const workspaceDir = vscode.workspace.getWorkspaceFolder(doc.uri);
+  const workspaceDir = vscode.workspace.getWorkspaceFolder(doc.uri);
 
-	return child_process.execSync("rustfmt", {
-		encoding: 'utf-8',
-		input: doc.getText(),
-		cwd: workspaceDir?.uri.fsPath,
-	}).toString();
+  return child_process
+    .execSync("rustfmt", {
+      encoding: "utf-8",
+      input: doc.getText(),
+      cwd: workspaceDir?.uri.fsPath
+    })
+    .toString();
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.languages.registerDocumentFormattingEditProvider('rust', {
-		provideDocumentFormattingEdits(doc: vscode.TextDocument): vscode.TextEdit[] {
-			return [vscode.TextEdit.replace(rangeWholeFile(doc), getFormattedString(doc))];
-		}
-	});
-	context.subscriptions.push(disposable);
+  let disposable = vscode.languages.registerDocumentFormattingEditProvider(
+    "rust",
+    {
+      provideDocumentFormattingEdits(
+        doc: vscode.TextDocument
+      ): vscode.TextEdit[] {
+        return [
+          vscode.TextEdit.replace(rangeWholeFile(doc), getFormattedString(doc))
+        ];
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
